dd60db805f1db8a37a9e2e4f78fe65ca21343313
Владимир Ворона <vvorona665@gmail.com>
2022-06-20 01:21:06 +0300

    Completed Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа которая на принимает на вход число и выдает сумму цифр в числе 
+
+Console.WriteLine("Введите число" );
+int Namber = Convert.ToInt32(Console.ReadLine());
+
+int Summa (int N)
+{
+    int res = 0; 
+    for (int i = 0; N > 0; i++)
+    {
+        int s = N % 10;
+        res = res + s;
+        N = N / 10;
+    }
+    return res;
+}
+    
+Console.WriteLine(Summa(Namber));
