47c69f4cd33873c711a8b44488d9f21c60920e44
Владимир Ворона <vvorona665@gmail.com>
2022-06-20 01:21:47 +0300

    Completed Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа которая задает массив из 8 элементов и выдает их на экран 
+
+int [] array = new int[8];
+
+void PrintArray(int [] array)
+{
+    int lenght = array.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(0, 199);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(array);
